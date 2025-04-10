c673c86d1eaa677eb6f9343768e1f911882ab634
Yasin Aydas <115567753+iysz18@users.noreply.github.com>
2025-04-10 09:29:57 +0200

    added button for prototyping the logic for later menu tab browsing.

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,12 @@
 import "../css/styles.css";
 import pizzaImage from "../assets/pizza.jpg";
 
-const image = document.createElement('img');
-image.src = pizzaImage;
+const renderImg = document.createElement("button");
+renderImg.addEventListener("click", () => {
+    const image = document.createElement('img');
+    image.src = pizzaImage;
+    
+    document.body.appendChild(image);
+});
 
-document.body.appendChild(image);
+document.body.appendChild(renderImg);
