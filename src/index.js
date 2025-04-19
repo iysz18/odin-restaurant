f78827eb169fd9df234c565a98da5358553a9695
Yasin Aydas <115567753+iysz18@users.noreply.github.com>
2025-04-19 15:50:10 +0200

    removed test code for creating elements.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
 import "./styles.css";
-import img from "./heroImg.jpg";
-import { greeting } from "../src/greeting.js";
-
-// references to elements
-const content = document.querySelector(".content");
-
-// creating elements for the home tab
-// the heroImage containing the image
-const foodImg = document.createElement("img");
-foodImg.src = img;
-
-// appending each created element to the contant container
-content.appendChild(foodImg);
 
 console.log(greeting);
