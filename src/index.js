e1a8a5bbe1fcb167eb5c451b148f598b083a70fd
Yasin Aydas <115567753+iysz18@users.noreply.github.com>
2025-04-19 14:25:15 +0200

    testing img imports.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import "./styles.css";
+import img from "./heroImg.jpg";
 import { greeting } from "../src/greeting.js";
+
+const content = document.querySelector(".content");
+const foodImg = document.createElement("img");
+foodImg.src = img;
+
+content.appendChild(foodImg);
 
 console.log(greeting);
