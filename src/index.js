3cac8c4ff51945bacbec72819c9f34e6924897d6
Yasin Aydas <115567753+iysz18@users.noreply.github.com>
2025-04-22 15:08:32 +0200

    added the event listener to check if the page laoded fully, then render the home content as default starting point.

--- src/index.js
+++ src/index.js
@@ -5,19 +5,24 @@ import "./nav.css";
 // importing modules to render each tab content
 import { renderHome } from "./home.js";
 
-// reference elements for tab browsing
-const contentContainer = document.querySelector(".content");
-const homeBtn = document.querySelector("#home-btn");
+document.addEventListener("DOMContentLoaded", ()  => {
+    // reference elements for tab browsing
+    const contentContainer = document.querySelector(".content");
+    const homeBtn = document.querySelector("#home-btn");
 
-// in later development, each button in the navbar will be
-// stored in a nodeList, for now to test this code, only home
-// button will have the eventlistener to render the home tab content
-homeBtn.addEventListener("click", () => {
-    // before proceeding to render the content, check if contentContainer is empty
-    if (contentContainer.innerHTML !== "") {
-        contentContainer.innerHTML = "";
-    }
-
-    // render the home tab content
+    // set home as default tab content thats loaded up as default at page load
     contentContainer.appendChild(renderHome());
+        
+    // in later development, each button in the navbar will be
+    // stored in a nodeList, for now to test this code, only home
+    // button will have the eventlistener to render the home tab content
+    homeBtn.addEventListener("click", () => {
+        // before proceeding to render the content, check if contentContainer is empty
+        if (contentContainer.innerHTML !== "") {
+            contentContainer.innerHTML = "";
+        }
+
+        // render the home tab content
+        contentContainer.appendChild(renderHome());
+    });
 });
