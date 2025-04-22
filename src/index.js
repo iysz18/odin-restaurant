5a93064ef2d0face70417ad851fc4ffd05547186
Yasin Aydas <115567753+iysz18@users.noreply.github.com>
2025-04-22 13:31:22 +0200

    applied the logic for the home tab content rendering, checking first if contentContainer is empty before renderning.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,24 @@ import "normalize.css"
 import "./styles.css";
 import "./nav.css";
 
+// importing modules to render each tab content
+import { renderHome } from "./home.js";
+
+// reference elements for tab browsing
+const contentContainer = document.querySelector(".content");
+const homeBtn = document.querySelector("#home-btn");
+
+// function to reset if possible the content of contentContainer
+ 
+// in later development, each button in the navbar will be
+// stored in a nodeList, for now to test this code, only home
+// button will have the eventlistener to render the home tab content
+homeBtn.addEventListener("click", () => {
+    // before proceeding to render the content, check if contentContainer is empty
+    if (contentContainer.innerHTML !== "") {
+        contentContainer.innerHTML = "";
+    }
+
+    // render the home tab content
+    contentContainer.appendChild(renderHome());
+});
