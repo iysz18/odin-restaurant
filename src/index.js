597a556d9cec46d9f2ed9a229223f834657172aa
Yasin Aydas <115567753+iysz18@users.noreply.github.com>
2025-04-27 11:00:15 +0200

    added simple solution to render menu tab content.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import "./nav.css";
 
 // importing modules to render each tab content
 import { renderHome } from "./home.js";
+import { renderMenu } from "./menu.js";
+
 
 document.addEventListener("DOMContentLoaded", ()  => {
+    // function handling reset of the contentContainer
+    const resetContainer = () => {
+        if (contentContainer.innerHTML != '') contentContainer.innerHTML = "";
+    }
+    
     // reference elements for tab browsing
     const contentContainer = document.querySelector(".content");
     const homeBtn = document.querySelector("#home-btn");
+    const menuBtn = document.querySelector("#menu-btn");
 
     // set home as default tab content thats loaded up as default at page load
     contentContainer.appendChild(renderHome());
@@ -18,11 +26,22 @@ document.addEventListener("DOMContentLoaded", ()  => {
     // button will have the eventlistener to render the home tab content
     homeBtn.addEventListener("click", () => {
         // before proceeding to render the content, check if contentContainer is empty
-        if (contentContainer.innerHTML !== "") {
-            contentContainer.innerHTML = "";
-        }
+        // if (contentContainer.innerHTML !== "") {
+        //     contentContainer.innerHTML = "";
+        // }
+
+        // reset the contentContainer with resetContainer()
+        resetContainer();
 
         // render the home tab content
         contentContainer.appendChild(renderHome());
     });
+
+    // rendering the menu
+    menuBtn.addEventListener("click", () => {
+        resetContainer();
+        // after resseting the contentContainer, render the menu
+        contentContainer.appendChild(renderMenu());
+    });
+
 });
