0a6991c7a3af7bd9e7361cc8b068e2f9607ea366
Yasin Aydas <115567753+iysz18@users.noreply.github.com>
2025-05-04 14:32:23 +0200

    renderMenu function renders the tab content of the menu.

--- src/menu.js
+++ src/menu.js
@@ -20,17 +20,38 @@ export function renderMenu() {
     const grid = document.createElement("div");
     grid.classList.add("grid-container");
 
+    let itemsArr = [];
+
     // create each menu item, append them to grid
     for (let i = 0; i < 8; i++) {
         const menuItem = document.createElement("div");
         menuItem.classList.add("menu-item");
-        menuItem.textContent = `Menu Item ${i}`;
+        // add menu item to imtesArr
+        itemsArr.push(menuItem);
         grid.appendChild(menuItem);
     }
 
+    // add to each card next its sibling grid element the name of the menu item
+    itemsArr[0].textContent = "Spice Kick Harvest Bowl";
+    itemsArr[0].classList.add("menu-item-name");
+    itemsArr[1].textContent = "A bold medley of roasted sweet potatoes, chili-lime chickpeas, charred corn, black beans, and quinoa, all drizzled with our signature smoky chipotle dressing. This bowl brings the heat and harmony in every bite.";
+    itemsArr[1].classList.add("menu-item-desc");
 
-    console.log(items);
-    
+    itemsArr[2].textContent = "Green Glow Buddha Bowl";
+    itemsArr[2].classList.add("menu-item-name");
+    itemsArr[3].textContent = "Packed with nutrient-rich kale, avocado, edamame, cucumber ribbons, and spirulina rice, this bowl is topped with a tangy ginger-miso glaze. Clean, green, and bursting with feel-good flavor.";
+    itemsArr[3].classList.add("menu-item-desc");
+
+    itemsArr[4].textContent = "Fire & Root Fusion";
+    itemsArr[4].classList.add("menu-item-name");
+    itemsArr[5].textContent = "Earthy roasted beets, crunchy radish slaw, and fiery gochujang tofu unite over jasmine rice, with a punchy sesame-lime vinaigrette. A grounding yet spicy experience that energizes your senses.";
+    itemsArr[5].classList.add("menu-item-desc");
+
+    itemsArr[6].textContent = "Green Glow Buddha Bowl";
+    itemsArr[6].classList.add("menu-item-name");
+    itemsArr[7].textContent = "Packed with nutrient-rich kale, avocado, edamame, cucumber ribbons, and spirulina rice, this bowl is topped with a tangy ginger-miso glaze. Clean, green, and bursting with feel-good flavor.";
+    itemsArr[7].classList.add("menu-item-desc");
+
     // append the sub text to the menuContainer
     menuContainer.appendChild(menuSubStr);
     menuContainer.appendChild(grid);
