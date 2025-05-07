e1b916be54ed59bd28dbe170f5be360311345761
Yasin Aydas <115567753+iysz18@users.noreply.github.com>
2025-05-07 11:55:09 +0200

    removed for refactoring the about tab content generation.

---
+++ src/about.js
@@ -0,0 +1,9 @@
+// module to render the about page
+import "normalize.css";
+import "./about.css";
+
+// export of the renderAbout function
+export function renderAbout() {
+
+    return aboutContainer;
+}
