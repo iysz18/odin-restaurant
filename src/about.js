7f31a7cb7715ea0ca02c2ee9f0cb2d20fa1a1c2e
Yasin Aydas <115567753+iysz18@users.noreply.github.com>
2025-05-07 16:24:46 +0200

    added logic to create multiple containers for each text section later.

--- src/about.js
+++ src/about.js
@@ -4,6 +4,35 @@ import "./about.css";
 
 // export of the renderAbout function
 export function renderAbout() {
+    // create sub containers to 
+    let containerArr = [];
+    for (let i = 0; i < 6; i++) {
+        const container = document.createElement("div");
+        container.classList.add("content-container");
+        containerArr.push(container);
+    }
+
+    
+
+    // create the container which contains each element for
+    // the representation of the about tab content 
+    const aboutContainer = document.createElement("div");
+    aboutContainer.classList.add("about-container");
+
+    // creating each element needed to reprensent the restaurant about
+    /* hero */
+    const aboutHero = document.createElement("h1");
+    aboutHero.textContent = "Who Are We?";
+    aboutHero.classList.add("about-hero");
+
+    /* restaurant introduction / misison statement */
+    const restaurantIntroduction = document.createElement("h2");
+    const restaurantIntroductionSub = document.createElement("p");
+    
+
+    
+    // append each element to the about container
+    aboutContainer.appendChild(aboutHero);
 
     return aboutContainer;
 }
